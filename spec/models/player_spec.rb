455c8534d9bfdb8ecdca6ac42b04df7d3a96c6a2
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2017-03-21 20:41:21 +0100

    Add player association to games

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
-RSpec.describe Player do
+RSpec.describe Player, type: :model do
+  describe 'associations' do
+    it { is_expected.to have_many(:games1).class_name('Game').dependent(:destroy) }
+    it { is_expected.to have_many(:games2).class_name('Game').dependent(:destroy) }
+  end
 end
