76284e5f908ea04c835e6374426b510327877614
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2017-03-21 23:13:51 +0100

    Add POST to create a game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,13 @@ class GamesController < ApplicationController
   get '/' do
     json Game.all.as_json
   end
+
+  post '/' do
+    game = Game.new(player1_id: params[:game][:player1_id], player2_id: params[:game][:player2_id])
+    if game.save
+      json game
+    else
+      halt 422, json(game.errors)
+    end
+  end
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -10,9 +10,27 @@ RSpec.describe GamesController do
     subject(:request) { get '/' }
 
     it 'returns all games' do
-      get "/"
+      request
 
       expect(last_response.body).to eq("[#{game.to_json}]")
+      expect(last_response.status).to eq 200
+    end
+  end
+
+  describe 'POST /games' do
+    let(:player1) { create(:player) }
+    let(:player2) { create(:player) }
+    subject(:request) { post '/', game: game }
+    let(:game) { { player1_id: player1, player2_id: player2 } }
+
+    it 'creates a game' do
+      expect{ request }.to change{ Game.count }.by 1
+    end
+
+    it 'returns all games' do
+      request
+
+      expect(last_response.body).to eq("#{Game.last.to_json}")
       expect(last_response.status).to eq 200
     end
   end
