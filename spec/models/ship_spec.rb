f6105d56964b6042110b71e888e91991e750b26c
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2017-03-21 23:23:01 +0100

    Add ship validation for length

--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -4,5 +4,8 @@ RSpec.describe Ship, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:length) }
+    it { is_expected.to validate_numericality_of(:length).only_integer
+                                                     .is_less_than_or_equal_to(10)
+                                                     .is_greater_than(0) }
   end
 end
