7a6b0a9c66987a9f469953769a87faddfd7b352c
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2017-03-21 18:55:27 +0100

    Create table players

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321175007) do
+ActiveRecord::Schema.define(version: 20170321175315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20170321175007) do
     t.boolean  "finished",   default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.datetime "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
