557e3103767e75bd73ffe839f69fc30c337799f8
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2017-03-21 22:55:10 +0100

    Add post ‘/players’ to create a player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,4 +3,13 @@ class PlayersController < ApplicationController
   get '/' do
     json Player.all
   end
+
+  post '/' do
+    player = Player.new(params[:player])
+    if player.save
+      json player
+    else
+      halt 422, json(player.errors)
+    end
+  end
 end
--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -16,4 +16,24 @@ RSpec.describe PlayersController do
       expect(last_response.status).to eq 200
     end
   end
+
+  describe 'POST /players/' do
+    subject(:request) { post '/', player: player  }
+    let(:player) { { name: name } }
+    let(:name) { 'Shreyas' }
+
+    it 'creates a player' do
+      expect{ request }.to change{ Player.count }.by 1
+    end
+
+    context 'when player cannot be created' do
+      let(:name) { '' }
+      it 'returns all players' do
+        request
+
+        expect(JSON.parse(last_response.body)).to eq({"name" => ["can't be blank"]})
+        expect(last_response.status).to eq 422
+      end
+    end
+  end
 end
