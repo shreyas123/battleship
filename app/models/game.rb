d39c1f3410b27c5aaba5d805fabc5bb8a2e4336e
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2017-03-21 23:02:05 +0100

    Add validation to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,8 @@ class Game < ActiveRecord::Base
   belongs_to :player1, class_name: 'Player'
   belongs_to :player2, class_name: 'Player'
 
+  validates :player1, :player2, :started_at, presence: true
+
   def serializable_hash(options = {})
     super(
         include: [:player1, :player2]
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe Game, type: :model do
     it { is_expected.to belong_to(:player2).class_name('Player') }
   end
 
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:player1) }
+    it { is_expected.to validate_presence_of(:player2) }
+    it { is_expected.to validate_presence_of(:started_at) }
+  end
+
   describe '#as_json' do
     let(:game) { create(:game) }
     it 'constains extra data' do
