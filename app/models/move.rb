fc58b94744bee1bf9b1f005fb698d906fdb8e52e
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2017-03-23 01:17:27 +0100

    Add validations to the move model

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,4 +1,10 @@
+require_relative './placement'
+
 class Move < ActiveRecord::Base
   belongs_to :game
   belongs_to :ship_sunk, class_name: 'Ship'
+
+  validates :game_id, :horizontal_move, :vertical_move, presence: true
+  validates :vertical_move, inclusion: { in: Placement::VERTICAL_NAMES }, uniqueness: { scope: [:horizontal_move, :game_id] }
+  validates :horizontal_move, inclusion: { in: Placement::HORIZONTAL_NAMES }, uniqueness: { scope: [:vertical_move, :game_id] }
 end
--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Move, type: :model do
     it { is_expected.to belong_to(:game) }
     it { is_expected.to belong_to(:ship_sunk).class_name('Ship') }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:game_id) }
+    it { is_expected.to validate_presence_of(:horizontal_move) }
+    it { is_expected.to validate_presence_of(:vertical_move) }
+    it { is_expected.to validate_uniqueness_of(:horizontal_move).scoped_to([:vertical_move, :game_id]) }
+    it { is_expected.to validate_uniqueness_of(:vertical_move).scoped_to([:horizontal_move, :game_id]) }
+    it { is_expected.to validate_inclusion_of(:vertical_move).in_array(Placement::VERTICAL_NAMES) }
+    it { is_expected.to validate_inclusion_of(:horizontal_move).in_array(Placement::HORIZONTAL_NAMES) }
+  end
 end
