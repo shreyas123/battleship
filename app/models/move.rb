e51bf86ea3171a8f051653f6210cffb00a45fe80
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2017-03-23 01:21:07 +0100

    Add validation to presence of a player_number in model

--- app/models/move.rb
+++ app/models/move.rb
@@ -4,7 +4,13 @@ class Move < ActiveRecord::Base
   belongs_to :game
   belongs_to :ship_sunk, class_name: 'Ship'
 
-  validates :game_id, :horizontal_move, :vertical_move, presence: true
-  validates :vertical_move, inclusion: { in: Placement::VERTICAL_NAMES }, uniqueness: { scope: [:horizontal_move, :game_id] }
-  validates :horizontal_move, inclusion: { in: Placement::HORIZONTAL_NAMES }, uniqueness: { scope: [:vertical_move, :game_id] }
+  has_many :placements, through: :game
+
+  validates :game_id, presence: true
+  validates :player_number, inclusion: { in: [1,2] }, presence: true
+  validates :vertical_move, presence: true, inclusion: { in: Placement::VERTICAL_NAMES }, uniqueness: { scope: [:horizontal_move, :game_id] }
+  validates :horizontal_move, presence: true, inclusion: { in: Placement::HORIZONTAL_NAMES }, uniqueness: { scope: [:vertical_move, :game_id] }
+
+
+
 end
--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -10,9 +10,15 @@ RSpec.describe Move, type: :model do
     it { is_expected.to validate_presence_of(:game_id) }
     it { is_expected.to validate_presence_of(:horizontal_move) }
     it { is_expected.to validate_presence_of(:vertical_move) }
+    it { is_expected.to validate_presence_of(:player_number) }
     it { is_expected.to validate_uniqueness_of(:horizontal_move).scoped_to([:vertical_move, :game_id]) }
     it { is_expected.to validate_uniqueness_of(:vertical_move).scoped_to([:horizontal_move, :game_id]) }
     it { is_expected.to validate_inclusion_of(:vertical_move).in_array(Placement::VERTICAL_NAMES) }
     it { is_expected.to validate_inclusion_of(:horizontal_move).in_array(Placement::HORIZONTAL_NAMES) }
+    it { is_expected.to validate_inclusion_of(:player_number).in_array([1, 2]) }
+  end
+
+  describe 'callbacks' do
+
   end
 end
